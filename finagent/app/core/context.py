70d71a370356bbbf325bb84ad87b667f32c3b2af
prady001 <gabriel.prady1@gmail.com>
2026-07-31 19:22:45 -0300

    Revert "merge: feature/issue-04b-logging-middleware (#04b)"
    This reverts commit 8208decaeba48b03df5ebc6e17c3bafe8bd3d12e, reversing
    changes made to 68bd60d0ee839ddae770b8ac93274e3eb1a3082c.

--- finagent/app/core/context.py
+++
@@ -1,5 +0,0 @@
-from contextvars import ContextVar
-
-
-current_pipeline_run_id: ContextVar[str | None] = ContextVar("current_pipeline_run_id", default=None)
-current_morning_note_id: ContextVar[str | None] = ContextVar("current_morning_note_id", default=None)
